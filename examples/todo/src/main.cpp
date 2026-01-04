d228712ec8989c96b21151917cb2bf648451700b
施从政 <shicongzheng@dcloud.io>
2026-01-04 20:27:24 +0800

    update exmaple

--- examples/todo/src/main.cpp
+++ examples/todo/src/main.cpp
@@ -3,34 +3,46 @@
 
 using namespace COMMANDER_CPP;
 
+
+class TestLogger : public Logger
+{
+  public:
+  TestLogger(){}
+    virtual Logger *debug(const String &msg)
+    {
+        std::cout << "DEBUG: " << msg << std::endl;
+        return this;
+    }
+    virtual Logger *warn(const String &msg)
+    {
+        std::cout << "WARN: " << msg << std::endl;
+        return this;
+    }
+    virtual Logger *error(const String &msg)
+    {
+        std::cout << "ERROR: " << msg << std::endl;
+        return this;
+    }
+    virtual Logger *print(const String &msg)
+    {
+        std::cout << "PRINT: " << msg << std::endl;
+        return this;
+    }
+};
 int main(int argc, char **argv)
 {
-    // TestLogger logger;
     // logger.stdOut = true;
-    Command("todo")
+    Command("todo", new TestLogger())
         .version("0.0.1")
         ->description("待办。")
-        ->addCommand((new Command("add"))
+        ->addCommand((new Command("add", nullptr))
                          ->description("添加一个新的待办事项。")
                          ->argument("<todo...>", "待办事项内容。")
                          ->action([](Command *cmd, Vector<Variant> args, Map<String, Variant> opts) {
-                             if (args.size() > 0)
-                             {
-                                 std::cout << "添加待办事项: ";
-                                 for (const auto &arg : std::get<std::vector<VariantBase>>(args[0]))
-                                 {
-                                     std::cout << std::get<String>(arg) << " ";
-                                 }
-                                 std::cout << std::endl;
-                             }
-                             else
-                             {
-                                 std::cout << "没有提供待办事项内容。" << std::endl;
-                             }
+                             std::cout << "这是一个待办事项应用程序的示例。" << std::endl;
                          }))
         // ->command("add <todo...>", "添加一个新的待办事项。")
         // ->action([](Command *cmd, Vector<Variant> args, Map<String, Variant> opts) {
-        //     // std::cout << "这是一个待办事项应用程序的示例。" << std::endl;
         //     std::cout << cmd->helpText() << std::endl;
         // })
         ->parse(argc, argv);
