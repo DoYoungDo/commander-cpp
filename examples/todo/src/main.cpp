e8f02bb0863c4a6714a3bc845af64ec98e083d91
Doyoung <doyoung9508@126.com>
2026-01-04 23:13:23 +0800

    update example

--- examples/todo/src/main.cpp
+++ examples/todo/src/main.cpp
@@ -38,13 +38,23 @@ int main(int argc, char **argv)
         ->addCommand((new Command("add", nullptr))
                          ->description("添加一个新的待办事项。")
                          ->argument("<todo...>", "待办事项内容。")
-                         ->action([](Command *cmd, Vector<Variant> args, Map<String, Variant> opts) {
-                             std::cout << "这是一个待办事项应用程序的示例。" << std::endl;
-                         }))
-        // ->command("add <todo...>", "添加一个新的待办事项。")
-        // ->action([](Command *cmd, Vector<Variant> args, Map<String, Variant> opts) {
-        //     std::cout << cmd->helpText() << std::endl;
-        // })
+                         ->option("-d --done", "将待办事项标记为已完成。")
+                         ->action([](Command *cmd, Vector<Variant> args, Map<String, Variant> opts)
+                                  { std::cout << cmd->helpText() << std::endl; }))
+        ->addCommand((new Command("rm", nullptr))
+                         ->description("删除一个待办事项。")
+                         ->argument("<index...>", "待办事项索引。")
+                         ->action([](Command *cmd, Vector<Variant> args, Map<String, Variant> opts)
+                                  { std::cout << cmd->helpText() << std::endl; }))
+        ->addCommand((new Command("mod", nullptr))
+                         ->description("修改一个待办事项。")
+                         ->option("-a --append", "追加内容到待办事项。")
+                         ->option("-d --done", "将待办事项标记为已完成。")
+                         ->argument("<index>", "待办事项索引。")
+                         ->argument("[todo]", "待办事项内容。")
+                         ->action([](Command *cmd, Vector<Variant> args, Map<String, Variant> opts)
+                                  { std::cout << cmd->helpText() << std::endl; }))
+
         ->parse(argc, argv);
     return 0;
 }
